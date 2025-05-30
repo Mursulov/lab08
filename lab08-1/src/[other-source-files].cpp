8f5cedb03ccccc2a63ae236e158b5b2e4a42b5c2
Maksim <78586711+Mursulov@users.noreply.github.com>
2025-05-30 18:55:37 +0300

    add

--- lab08-1/src/[other-source-files].cpp
+++
@@ -1,2 +0,0 @@
-// filepath: lab08/src/[other-source-files].cpp
-// This file is intentionally left blank.
--- lab08-1/tests/test.cpp
+++
@@ -1,9 +0,0 @@
-#include <gtest/gtest.h>
-
-// Sample test case
-TEST(SampleTest, BasicAssertions) {
-    EXPECT_EQ(1, 1);
-    EXPECT_TRUE(true);
-}
-
-// Add more test cases as needed
